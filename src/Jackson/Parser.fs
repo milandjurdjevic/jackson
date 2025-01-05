205a4f322bf2b4dbfddd381009204b210924c2d7
Milan Djurdjevic <milan_djurdjevic@icloud.com>
2025-01-05 01:57:54 +0100

    Remove ParserExtensions & Refactor

--- src/Jackson/Parser.fs
+++ src/Jackson/Parser.fs
@@ -5,80 +5,70 @@ open System.Linq
 open System.Text.Json
 open System.Globalization
 open System.Text.Json.Nodes
-open System.Runtime.CompilerServices
 
-module internal Parser =
-    let dynamic (node: JsonNode) =
-        let toNum (number: string) =
-            let culture = CultureInfo.InvariantCulture
+module internal DynamicParser =
+    let private culture = CultureInfo.InvariantCulture
 
-            let tOption (this: bool * 'a) =
-                if fst this then Some(snd this) else None
+    let private run (parser: string * CultureInfo -> bool * 'a) value =
+        let ok, result = parser (value, culture)
+        if ok then Option.Some result else Option.None
 
-            Byte.TryParse(number, culture)
-            |> tOption
-            |> Option.map box
-            |> Option.orElseWith (fun () -> Int16.TryParse(number, culture) |> tOption |> Option.map box)
-            |> Option.orElseWith (fun () -> Int32.TryParse(number, culture) |> tOption |> Option.map box)
-            |> Option.orElseWith (fun () -> Int64.TryParse(number, culture) |> tOption |> Option.map box)
-            |> Option.orElseWith (fun () ->
-                Single.TryParse(number, culture)
-                |> tOption
-                |> Option.filter Single.IsFinite
-                |> Option.map box)
-            |> Option.orElseWith (fun () ->
-                Double.TryParse(number, culture)
-                |> tOption
-                |> Option.filter Double.IsFinite
-                |> Option.map box)
+    let private parseNumber number =
+        run Byte.TryParse number
+        |> Option.map box
+        |> Option.orElseWith (fun () -> run Int16.TryParse number |> Option.map box)
+        |> Option.orElseWith (fun () -> run Int32.TryParse number |> Option.map box)
+        |> Option.orElseWith (fun () -> run Int64.TryParse number |> Option.map box)
+        |> Option.orElseWith (fun () -> run Single.TryParse number |> Option.filter Single.IsFinite |> Option.map box)
+        |> Option.orElseWith (fun () -> run Double.TryParse number |> Option.filter Double.IsFinite |> Option.map box)
 
-        let toObj (value: JsonValue) : obj =
-            match value.GetValueKind() with
-            | JsonValueKind.String -> value.GetValue<string>()
-            | JsonValueKind.Number -> value.ToString() |> toNum |> Option.get
-            | JsonValueKind.True -> true
-            | JsonValueKind.False -> false
-            | JsonValueKind.Undefined -> null
-            | JsonValueKind.Null -> null
-            | _ -> invalidOp $"Unknown value kind: {value.ToString()}."
+    let private parseValue (value: JsonValue) : obj =
+        match value.GetValueKind() with
+        | JsonValueKind.String -> value.GetValue<string>()
+        | JsonValueKind.Number -> value.ToString() |> parseNumber |> Option.get
+        | JsonValueKind.True -> true
+        | JsonValueKind.False -> false
+        | JsonValueKind.Undefined -> null
+        | JsonValueKind.Null -> null
+        | _ -> invalidOp $"Unknown value kind: {value.ToString()}."
 
-        // BUG: StackOverflowException
-        // Convert function to tail-recursive, so it can handle large JSON files.
-        let rec dynamic (node: JsonNode) =
-            match node with
-            | :? JsonValue as value -> toObj value
-            | :? JsonObject as object -> object |> Seq.map (fun n -> n.Key, dynamic n.Value) |> readOnlyDict |> box
-            | :? JsonArray as array -> array |> Seq.map dynamic |> box
-            | _ -> null
+    let rec private parseNode (node: JsonNode) =
+        match node with
+        | :? JsonValue as value -> parseValue value
+        | :? JsonObject as object -> object |> Seq.map (fun n -> n.Key, parseNode n.Value) |> readOnlyDict |> box
+        | :? JsonArray as array -> array |> Seq.map parseNode |> box
+        | _ -> null
 
-        match dynamic node with
+    let parse (node: JsonNode) =
+        match parseNode node with
         | :? seq<obj> as seq -> seq :> obj
         | obj -> obj
 
+module internal MappedParser =
     type private TypeMap = string array * Map<string, Type>
 
-    let mapped (map: TypeMap) (options: JsonSerializerOptions) (next: JsonNode -> obj) (node: JsonNode) =
-        let typeOf (obj: JsonObject) =
-            let valOf key =
-                obj
-                |> Seq.tryFind (_.Key.Equals(key, StringComparison.OrdinalIgnoreCase))
-                |> Option.map (_.Value.ToString())
+    let private findValue (obj: JsonObject) key =
+        obj
+        |> Seq.tryFind (_.Key.Equals(key, StringComparison.OrdinalIgnoreCase))
+        |> Option.map (_.Value.ToString())
 
-            fst map
-            |> Seq.map valOf
-            |> Seq.filter Option.isSome
-            |> Seq.map Option.get
-            |> String.concat String.Empty
-            |> (snd map).TryFind
+    let private findType (types: TypeMap) (obj: JsonObject) =
+        fst types
+        |> Seq.map (findValue obj)
+        |> Seq.filter Option.isSome
+        |> Seq.map Option.get
+        |> String.concat String.Empty
+        |> (snd types).TryFind
 
-        let toObj obj =
-            typeOf obj
-            |> Option.map (fun t -> obj.Deserialize(t, options))
-            |> Option.defaultValue (next obj)
+    let parse (map: TypeMap) (options: JsonSerializerOptions) (next: JsonNode -> obj) (node: JsonNode) =
+        let parseObject object =
+            findType map object
+            |> Option.map (fun t -> object.Deserialize(t, options))
+            |> Option.defaultValue (next object)
 
         match node with
-        | :? JsonArray as arr -> arr.OfType<JsonObject>() |> Seq.map toObj :> obj
-        | :? JsonObject as obj -> toObj obj
+        | :? JsonArray as arr -> arr.OfType<JsonObject>() |> Seq.map parseObject :> obj
+        | :? JsonObject as obj -> parseObject obj
         | _ -> Seq.empty
 
 type IParser =
@@ -97,17 +87,8 @@ type ParserBuilder
         ParserBuilder(keys, map, serializer, Some next)
 
     member _.Build() : IParser =
-        let parser =
-            Parser.mapped (keys, map) serializer (next |> Option.map (_.Parse) |> Option.defaultValue Parser.dynamic)
+        let next = next |> Option.map (_.Parse) |> Option.defaultValue DynamicParser.parse
+        let parser = MappedParser.parse (keys, map) serializer next
 
         { new IParser with
             member _.Parse node = parser node }
-
-[<AbstractClass; Sealed>]
-type ParserExtensions() =
-
-    [<Extension>]
-    static member CreateParserBuilder(options, [<ParamArray>] keys: string array) = ParserBuilder.Create(options, keys)
-
-    [<Extension>]
-    static member Chain(parser: IParser, builder: ParserBuilder) = builder.Chain(parser)
